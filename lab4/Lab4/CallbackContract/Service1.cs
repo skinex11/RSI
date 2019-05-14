98360f0794aa1df010e6e819e957ec4f3b48260d
skinex <skinex11@gmail.com>
2019-05-14 19:28:08 +0200

    Czesc 8

--- lab4/Lab4/CallbackContract/Service1.cs
+++ lab4/Lab4/CallbackContract/Service1.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Threading;
 
 namespace CallbackContract
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    [ServiceBehavior(InstanceContextMode=InstanceContextMode.PerSession)]
+    public class mojCallbackKalkulator : ICallbackKalkulator
     {
         public string GetData(int value)
         {
@@ -26,6 +28,36 @@ namespace CallbackContract
                 composite.StringValue += "Suffix";
             }
             return composite;
+        }
+
+        double result;
+        ICallbackHandler callback = null;
+
+        public mojCallbackKalkulator()
+        {
+            callback = OperationContext.Current.GetCallbackChannel<ICallbackHandler>();
+        }
+
+        public void ObliczCos(int sek)
+        {
+            Console.WriteLine("...wywolano Oblicz({0})", sek);
+            if (sek < 10)
+                Thread.Sleep(sek * 1000);
+            else
+                Thread.Sleep(1000);
+            callback.ZwrotObliczCos("Obliczenia trwaly " +
+                (sek + 1) +
+                " sekund(y)");
+        }
+
+        public void Silnia(double n)
+        {
+            Console.WriteLine("...wywolano Silnia({0})", n);
+            Thread.Sleep(1000);
+            result = 1;
+            for (int i = 1; i <= n; i++)
+                result *= i;
+            callback.ZwrotSilnia(result);
         }
     }
 }
