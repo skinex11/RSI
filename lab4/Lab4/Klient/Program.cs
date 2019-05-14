a9975a3e92caee231eae9bbc98bd799991118391
skinex <skinex11@gmail.com>
2019-05-14 19:05:38 +0200

    Czesc 4

--- lab4/Lab4/Klient/Program.cs
+++ lab4/Lab4/Klient/Program.cs
@@ -1,8 +1,11 @@
+using Klient.ServiceReference1;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using WcfServiceLibrary1;
 
 namespace Klient
 {
@@ -12,8 +15,29 @@ namespace Klient
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+
+            Service1Client client1 = new Service1Client("WSHttpBinding_IService1");
+
+            Console.WriteLine("...wywoluje funkcja1:");
+            client1.Funkcja1("Klient1");
+            Thread.Sleep(10);
+            Console.WriteLine("...kontynuacja po funkcji 1");
+
+            Console.WriteLine("...wywoluje funkcja2:");
+            client1.Funkcja2("Klient1");
+            Thread.Sleep(10);
+            Console.WriteLine("...kontynuacja po funkcji 2");
+
+            Console.WriteLine("...wywoluje funkcja1:");
+            client1.Funkcja1("Klient1");
+            Thread.Sleep(10);
+            Console.WriteLine("...kontynuacja po funkcji 1");
+
+            client1.Close();
+
+
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
