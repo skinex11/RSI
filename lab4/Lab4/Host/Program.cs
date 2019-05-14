16fe8c255d578f7270d7af38f0f0616a37c2ee44
skinex <skinex11@gmail.com>
2019-05-14 18:44:59 +0200

    Czesc 3

--- lab4/Lab4/Host/Program.cs
+++ lab4/Lab4/Host/Program.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using WcfServiceLibrary1;
 
 namespace Host
 {
@@ -12,8 +15,33 @@ namespace Host
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+
+            Uri baseAddress1 = new Uri("http://localhost:10000/mojhost");
+            ServiceHost mojHost1 = new ServiceHost(typeof(MojSerwis),
+                baseAddress1);
+            try
+            {
+                ServiceEndpoint endpoint1 = mojHost1.AddServiceEndpoint(typeof(IService1),
+                    new WSHttpBinding(),
+                    "endpoint1");
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                //Metadane:
+                smb.HttpGetEnabled = true;
+                mojHost1.Description.Behaviors.Add(smb);
+                mojHost1.Open();
+                Console.WriteLine("-->MojSerwis jest uruchomiony");
+                Console.WriteLine("-->Nacisinij <ENTER> aby zakonczyc.\n");
+                Console.ReadLine(); //czekam na zamkniecie
+                mojHost1.Close();
+                Console.WriteLine("-->Serwis zakonczyl dzialanie.");
+            }
+            catch(CommunicationException ce)
+            {
+                Console.WriteLine("Wystapil wyjatek: {0}", ce.Message);
+                mojHost1.Abort();
+            }
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
