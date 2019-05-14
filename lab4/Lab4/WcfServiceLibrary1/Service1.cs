5decabeb2002114a1de3a26ea0979c5fdc179220
skinex <skinex11@gmail.com>
2019-05-14 18:28:26 +0200

    Cześć 2

--- lab4/Lab4/WcfServiceLibrary1/Service1.cs
+++ lab4/Lab4/WcfServiceLibrary1/Service1.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Threading;
 
 namespace WcfServiceLibrary1
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    [ServiceBehavior(ConcurrencyMode=ConcurrencyMode.Multiple)]
+    public class MojSerwis : IService1
     {
+        public void Funkcja1(string s1)
+        {
+            Console.WriteLine("...{0}: funkcja1 - start", s1);
+            Thread.Sleep(3000);
+            Console.WriteLine("...{0}: funkcja1 - stop", s1);
+            return;
+        }
+
+        public void Funkcja2(string s2)
+        {
+            Console.WriteLine("...{0}: funkcja1 - start", s2);
+            Thread.Sleep(3000);
+            Console.WriteLine("...{0}: funkcja1 - stop", s2);
+            return;
+        }
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
