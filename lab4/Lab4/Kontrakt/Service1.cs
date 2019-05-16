fc8f27056d9541c047e694a9865cf1a265ea3995
skinex <skinex11@gmail.com>
2019-05-16 17:13:26 +0200

    Zrobione projekty dla części B

---
+++ lab4/Lab4/Kontrakt/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Kontrakt
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IStrumien
+    {
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public Stream GetStream(string nazwa)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
