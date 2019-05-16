4a86e828b1bc2a172ea982623079dce76896a088
skinex <skinex11@gmail.com>
2019-05-16 17:21:35 +0200

    Część 13

--- lab4/Lab4/Kontrakt/Service1.cs
+++ lab4/Lab4/Kontrakt/Service1.cs
@@ -11,6 +11,27 @@ namespace Kontrakt
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class Service1 : IStrumien
     {
+        public Stream GetStream(string nazwa)
+        {
+            FileStream myFile;
+            Console.WriteLine("-->Wywolano getStream");
+            string filePath = Path.Combine(System.Environment.CurrentDirectory,
+                ".\\image.jpg");
+            //wyjatek na wypadek bledu otwarcia pliku
+            try
+            {
+                myFile = File.OpenRead(filePath);
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(String.Format("Wyjatek otwarcia pliku {0} :",
+                    filePath));
+                Console.WriteLine(ex.ToString());
+                throw ex;
+            }
+            return myFile;
+        }
+
         public CompositeType GetDataUsingDataContract(CompositeType composite)
         {
             if (composite == null)
@@ -22,11 +43,6 @@ namespace Kontrakt
                 composite.StringValue += "Suffix";
             }
             return composite;
-        }
-
-        public Stream GetStream(string nazwa)
-        {
-            throw new NotImplementedException();
         }
     }
 }
